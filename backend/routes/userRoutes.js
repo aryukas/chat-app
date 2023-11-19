bb96ca1773fb3703635ae63e7af645cea5d1b0ac
arvind2602 <arvindgupta@Arvinds-MacBook-Air.local>
2023-11-19 11:59:08 +0530

    removed errors

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const { registerUser, authUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.post("/",registerUser);
-router.post("/login",authUser); 
+router.post("/",function(req, res){registerUser});
+router.post("/login",function(req, res){authUser}); 
 
 module.exports = router;
